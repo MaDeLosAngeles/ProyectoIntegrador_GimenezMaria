ced5af75cb4f4620684bc0fcdad921e5d642448f
Maria <mdla_gimenez@hotmail.com>
2022-05-21 20:06:48 -0300

    components app

--- ProyectoIntegrador_GimenezMaria/src/app/app.module.ts
+++ ProyectoIntegrador_GimenezMaria/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AcercaDeComponent,
+    BannerComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
     ],
   imports: [
     BrowserModule
