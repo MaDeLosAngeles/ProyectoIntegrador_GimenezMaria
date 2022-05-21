581ca4ea3c8f8b20e2b595db0facf4873a1da159
Maria <mdla_gimenez@hotmail.com>
2022-05-21 18:44:08 -0300

    acercaDe

--- ProyectoIntegrador_GimenezMaria/src/app/app.module.ts
+++ ProyectoIntegrador_GimenezMaria/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent
-  ],
+    AcercaDeComponent,
+    ],
   imports: [
     BrowserModule
   ],
