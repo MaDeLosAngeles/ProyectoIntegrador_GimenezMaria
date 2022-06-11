d8881e78829ab3d37db4a0a924ec9cf5f2f32ec4
Maria <mdla_gimenez@hotmail.com>
2022-06-10 21:58:06 -0300

    avances en head y comp experiencia y educac

--- ProyectoIntegrador_GimenezMaria/src/app/app.module.ts
+++ ProyectoIntegrador_GimenezMaria/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { SocialComponent } from './components/social/social.component';
     HeaderComponent,
     LogoApComponent,
     SocialComponent,
+    ExperienciaComponent,
+    EducacionComponent,
     ],
   imports: [
     BrowserModule
