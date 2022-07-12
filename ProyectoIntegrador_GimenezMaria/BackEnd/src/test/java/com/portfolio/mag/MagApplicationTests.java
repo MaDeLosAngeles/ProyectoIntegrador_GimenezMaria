ad004c721593b5cd8fa0ff7d46aa09ef29996b7b
Maria <mdla_gimenez@hotmail.com>
2022-07-12 18:53:24 -0300

    BackEnd

---
+++ ProyectoIntegrador_GimenezMaria/BackEnd/src/test/java/com/portfolio/mag/MagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
