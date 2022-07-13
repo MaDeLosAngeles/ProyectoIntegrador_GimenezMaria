1c51b8a0551bc47bdf038b4d375bc44a344a9e0a
Maria <mdla_gimenez@hotmail.com>
2022-07-13 13:14:17 -0300

    Routing

--- ProyectoIntegrador_GimenezMaria/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_GimenezMaria/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,9 +32,13 @@ import {HttpClientModule} from '@angular/common/http';
     HysComponent,
     ProyectoComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
     ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
