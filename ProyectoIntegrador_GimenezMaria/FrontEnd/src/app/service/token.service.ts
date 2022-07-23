15e66936f6a68c79b4b2fff3414153fc76bb8d84
Maria <mdla_gimenez@hotmail.com>
2022-07-23 12:54:55 -0300

    model y service

---
+++ ProyectoIntegrador_GimenezMaria/FrontEnd/src/app/service/token.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { triggerAsyncId } from 'async_hooks';
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUserName';
+const ATHORITIES_KEY = 'AuthAuthorities'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  roles: Array<string> = []
+
+  constructor() { }
+
+  public setToken(token:string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+
+  }
+
+  public getToken(): string {
+    return sessionStorage.getItem(TOKEN_KEY)!;
+  }
+
+  public setUserName(userName: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, userName);
+  }
+
+  public getUserName(): string {
+    return sessionStorage.getItem(USERNAME_KEY)!;
+  }
+
+  public setAuthorities(authorities: string[]):  void{
+    window.sessionStorage.removeItem(ATHORITIES_KEY);
+    window.sessionStorage.setItem(ATHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): string[]{
+    this.roles = [];
+    if(sessionStorage.getItem(ATHORITIES_KEY)!){
+       JSON.parse(sessionStorage.getItem(ATHORITIES_KEY)!).forEach((authority:any) => {
+        this.roles.push(authority.authority);
+       });
+    }
+    return this.roles;
+  }
+
+  public logOut(): void{
+    window.sessionStorage.clear();
+  }
+}
